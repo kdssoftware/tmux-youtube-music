069e644c8113f05d8db1fe20cd7d89d0d5d1997d
karel.de.smet <karel.de.smet@guardsquare.com>
2024-04-11 11:45:02 +0200

    inline applescript instead of dedicated applescript

--- scripts/track.sh
+++ scripts/track.sh
@@ -5,9 +5,40 @@ source "$CURRENT_DIR/helpers.sh"
 
 track=""
 
+music_track() {
+read -r -d '' SCRIPT <<END
+tell application "Google Chrome"
+    set tabNames to {}
+    set titleOfPlayingSong to ""
+    
+    repeat with w in windows
+        repeat with t in tabs of w
+            set tabTitle to title of t
+            if tabTitle ends with "YouTube Music" then
+                tell t
+                    set YTtitle to execute javascript "var element = document.querySelector('.title.style-scope.ytmusic-player-bar'); if (element) {element.getAttribute('title')} else {'Nothing playing'}"
+                end tell
+                tell t
+                    set YTbyline to execute javascript "var element = document.querySelector('.byline.style-scope.ytmusic-player-bar'); if (element) {element.getAttribute('title')} else {'Unknown artist'}"
+                end tell
+            end if
+        end repeat
+    end repeat
+end tell
+
+if YTtitle is not equal to "" then
+    return YTtitle & " by " & YTbyline
+else
+    return "Nothing playing"
+end if
+END
+
+osascript -e "${SCRIPT}"
+}
+
 print_music_track() {
   local trackInfo
-  trackInfo=$(osascript "./track.applescript")
+  trackInfo=$(music_track)
   # Remove any unneeded strings
   trackInfo=$(echo "$trackInfo" | sed -E 's/• .+ views//g; s/• .+ likes//g; s/ - YouTube Music//g; s/, YouTube Music//g; s/YouTube Music, //g')
   # trim spaces
